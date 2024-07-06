d8d7dafa89e642c4bc90df2ce52aef674e002d14
flex021 <congthong2015@gmail.com>
2024-07-06 22:53:24 +0700

    env setup

---
+++ src/server.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CLOSE_DB, CONNECT_DB } from '~/config/mongodb'
+
+const START_SERVER = () => {
+  const app = express()
+
+  const hostname = 'localhost'
+  const port = 8017
+
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello Thong, I am running at ${ hostname }:${ port }/`)
+  })
+
+  exitHook(() => {
+    console.log('4. close nè')
+    CLOSE_DB()
+    console.log('5. close nè')
+  })
+}
+
+// chỉ khi kết nối database thành công thì mới start server back-end lên
+(async () => {
+  try {
+    console.log('1. connecting to MongoDB Cloud Atlas')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
