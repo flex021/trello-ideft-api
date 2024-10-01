fb70a394a57ec8581a147e30eac766ff01899d0e
flex021 <congthong2015@gmail.com>
2024-10-01 12:04:20 +0700

    update cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
+  // không cần localhost nữa vì file cors.js đã luôn luôn cho phép môi trường dev
 ]
 
 export const BOARD_TYPES = {
