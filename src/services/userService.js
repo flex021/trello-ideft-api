901a13365d77e6603aaeb179dee4af0f9ddd58d4
flex021 <congthong2015@gmail.com>
2025-05-17 22:44:01 +0700

    check ENV 2

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,8 +10,8 @@ import { JwtProvider } from '~/providers/JwtProvider'
 import { env } from '~/config/environment'
 import { CloudinaryProvider } from '~/providers/CloudinaryProvider'
 
-console.log('ENV from process.env:', process.env.ACCESS_TOKEN_SECRET_SIGNATURE);
-console.log('ENV from env object:', env.ACCESS_TOKEN_SECRET_SIGNATURE);
+console.log('DEBUG CHECK - env.ACCESS_TOKEN_SECRET_SIGNATURE:', env.ACCESS_TOKEN_SECRET_SIGNATURE);
+console.log('DEBUG CHECK - process.env.ACCESS_TOKEN_SECRET_SIGNATURE:', process.env.ACCESS_TOKEN_SECRET_SIGNATURE);
 const createNew = async (reqBody) => {
   try {
     const existUser = await userModel.findOneByEmail(reqBody.email)
