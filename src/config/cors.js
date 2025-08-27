ccf7cd41ecba185682b3a6345fab090d3e819b34
flex021 <congthong2015@gmail.com>
2025-08-27 14:27:40 +0700

    Fix config imports and add dist

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,8 @@
 
-import { WHITELIST_DOMAINS } from '~/utils/constants'
-import { env } from '~/config/environment'
+import { WHITELIST_DOMAINS } from '../utils/constants'
+import { env } from '../config/environment'
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
+import ApiError from '../utils/ApiError'
 
 export const corsOptions = {
   origin: function (origin, callback) {
